4473a9f925fa1c0b9bc79f0a43d29760d0d17332
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-21 20:41:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,14 @@
 
 def key_for_min_value(name_hash)
   value_compare = nil
-  min_key = nil
-  name_hash.each do |key, value|
+  min_key = []
+  name_hash.collect do |key, value|
     if value_compare == nil && value != nil
       value_compare = value
-      min_key = key
+      min_key[0] = key
     elsif value < value_compare
       value_compare = value
-      min_key = key
+      min_key=[0]
   end
 end
   min_key
